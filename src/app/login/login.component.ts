fbd8c14ec86412bc69302534d6d3adb611cdb67b
Tanya Yusubova <44925800+Yusubovatanya@users.noreply.github.com>
2019-11-11 12:09:42 +0200

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  loginForm = { username: '', password: '' };
+  loginForm = { username: '', password: '', type: 'local' };
   errorMessage = '';
 
   constructor(
